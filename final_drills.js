ecf9493c09a4914885d3e3bf6fe0871587a2acdb
Chris Martin <chris.bizunit34@gmail.com>
2019-06-05 15:29:51 -0400

    add object initializers and methods

---
+++ final_drills.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// //Object initializers and methods
+// let loaf = {
+//   flour: 300,
+//   water: 210,
+//   hydration: function() {return this.water / this.flour * 100;}
+// };
+// console.log(`${loaf.flour} and ${loaf.water}`);
+// console.log(loaf.hydration());
+
+//Iterating over an object's properties
