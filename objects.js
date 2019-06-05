f6bc6afa36fef5bc88c5e74f7c48f23dbf5b6750
Mantong Zhao <mantong.cn@gmail.com>
2019-06-05 09:17:44 -0700

    drill2

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 'use strict';
-
-function createMyObject() {
+//drill 1
+/*function createMyObject() {
   // your code here
   return {
     foo: 'bar',
@@ -18,7 +18,7 @@ function createMyObject() {
      
      Nothing to see here!
      
-  */
+  
   
 (function testCreateMyObject() {
   var obj = createMyObject();
@@ -58,4 +58,61 @@ function createMyObject() {
     return false;
   }
   console.log('SUCCESS: Your function works!');
+})();
+*/
+
+
+//drill 2
+
+function updateObject(obj) {
+  obj.foo= 'foo';
+  obj.bar='bar';
+  obj.bizz='bizz';
+  obj.bang='bang'; 
+  return obj;
+}
+
+
+
+// your code here
+
+
+
+
+/* From here down, you are not expected to 
+   understand.... for now :)  
+   
+   
+   Nothing to see here!
+   
+*/
+
+(function testUpdateObject() {
+  var oldObj = {
+    cats: 'cats',
+    dogs: 'dogs',
+  };
+  var newObj = updateObject(oldObj);
+  if (typeof newObj !== 'object') { 
+    console.error('ERROR: `createMyObject` must return an object');
+    return false;
+  }
+  ['foo', 'bar', 'bizz', 'bang'].forEach(function(key) {
+    if (!(key in newObj)) {
+      console.error('ERROR: `' + key + '` not in object returned by `updateObject`');
+      return false;
+    }
+  });
+  ['foo', 'bar', 'bizz', 'bang'].forEach(function(key) {
+    if (newObj[key] !== key) {
+      console.error('ERROR: `' + key + '` should be "' + key + '" but was ' + newObj[key]);
+      return false;
+    }
+  });
+  if (!(newObj.cats === 'cats' && newObj.dogs === 'dogs')) {
+    console.error('ERROR: your function doesn\'t preserve existing key/value pairs');
+    return false;
+  }
+  console.log('SUCCESS: `updateObject` works correctly!');
+  
 })();
