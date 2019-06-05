31a4bfd76e8798718fb53b11ccff491aca07532f
Chris Martin <chris.bizunit34@gmail.com>
2019-06-05 14:40:31 -0400

    add Find by ID

--- newdoc.js
+++ newdoc.js
@@ -62,90 +62,142 @@
   
 // testIt();
   
-//drill2
+// //drill2
 
-const studentData = [
-  {
-    name: 'Tim',
-    status: 'Current student',
-    course: 'Biology',
-  },
-  {
-    name: 'Sue',
-    status: 'Withdrawn',
-    course: 'Mathematics',
-  },
-  {
-    name: 'Liz',
-    status: 'On leave',
-    course: 'Computer science',
-  },
-];
+// const studentData = [
+//   {
+//     name: 'Tim',
+//     status: 'Current student',
+//     course: 'Biology',
+//   },
+//   {
+//     name: 'Sue',
+//     status: 'Withdrawn',
+//     course: 'Mathematics',
+//   },
+//   {
+//     name: 'Liz',
+//     status: 'On leave',
+//     course: 'Computer science',
+//   },
+// ];
   
-function enrollInSummerSchool(students) {
-  let results = [];
-  for(let i=0;i<students.length;i++){
-    results.push(students[i]);
-    results.push(students[i].status='In Summer school');
-  }
+// function enrollInSummerSchool(students) {
+//   let results = [];
+//   for(let i=0;i<students.length;i++){
+//     results.push(students[i]);
+//     results.push(students[i].status='In Summer school');
+//   }
   
-  return results;
+//   return results;
  
-  // your code here
-}
+//   // your code here
+// }
   
-/* From here down, you are not expected to understand.... for now :)
-    Nothing to see here!
+// /* From here down, you are not expected to understand.... for now :)
+//     Nothing to see here!
   
-  */
+//   */
   
-// tests
+// // tests
   
-function testIt() {
-  var testData = [
-    {
-      name: 'Burt',
-      status: 'Playing hooky',
-      course: 'Biology',
-    },
-    {
-      name: 'Melanie',
-      status: 'Sick',
-      course: 'Mathematics',
-    },
-    {
-      name: 'Leonard',
-      status: 'AWOL',
-      course: 'Computer science',
-    },
-  ];
+// function testIt() {
+//   var testData = [
+//     {
+//       name: 'Burt',
+//       status: 'Playing hooky',
+//       course: 'Biology',
+//     },
+//     {
+//       name: 'Melanie',
+//       status: 'Sick',
+//       course: 'Mathematics',
+//     },
+//     {
+//       name: 'Leonard',
+//       status: 'AWOL',
+//       course: 'Computer science',
+//     },
+//   ];
   
-  var results = enrollInSummerSchool(testData);
+//   var results = enrollInSummerSchool(testData);
   
-  if (!(results && results instanceof Array)) {
-    console.error('FAILURE: `enrollSummerSchool` must return an array');
-    return;
-  }
-  var result = testData.every(function(student) {
-    var match = results.find(function(_student) {
-      return (
-        _student.name === student.name &&
-          _student.course === student.course &&
-          _student.status.toLowerCase() === 'in summer school'
-      );
-    });
-    return match !== undefined;
-  });
-  if (!result) {
-    console.error(
-      'FAILURE: `enrollSummerSchool` should return ' +
-          'original key/value pairs for each student, and ' +
-          'update `status` to "In Summer school": ' +
-          JSON.stringify(results)
-    );
-  } else {
-    console.info('SUCCESS: `enrollSummerSchool` is working');
-  }
-}
+//   if (!(results && results instanceof Array)) {
+//     console.error('FAILURE: `enrollSummerSchool` must return an array');
+//     return;
+//   }
+//   var result = testData.every(function(student) {
+//     var match = results.find(function(_student) {
+//       return (
+//         _student.name === student.name &&
+//           _student.course === student.course &&
+//           _student.status.toLowerCase() === 'in summer school'
+//       );
+//     });
+//     return match !== undefined;
+//   });
+//   if (!result) {
+//     console.error(
+//       'FAILURE: `enrollSummerSchool` should return ' +
+//           'original key/value pairs for each student, and ' +
+//           'update `status` to "In Summer school": ' +
+//           JSON.stringify(results)
+//     );
+//   } else {
+//     console.info('SUCCESS: `enrollSummerSchool` is working');
+//   }
+// }
   
-testIt();
+// testIt();
+
+// //Find by ID
+
+// // you can pass in `scratchData` to test out `findByid`
+// // your function
+// const scratchData = [
+//   { id: 22, foo: 'bar' },
+//   { id: 28, foo: 'bizz' },
+//   { id: 19, foo: 'bazz' },
+// ];
+  
+// function findById(items, idNum) {
+//   // your code here
+//   let result = {};
+//   for(let i=0; i<items.length;i++) {
+//     if(idNum===items[i].id){
+//       result = items[i];
+//     }
+//   }
+//   return result;
+// }
+  
+// //
+  
+// function testIt() {
+//   const testData = [
+//     { id: 1, foo: 'bar' },
+//     { id: 2, foo: 'bizz' },
+//     { id: 3, bang: 'boo' },
+//   ];
+//   const result = findById(testData, 3);
+//   if (!(result && result !== null && typeof result === 'object')) {
+//     console.error('`findById` must return an object');
+//     return;
+//   }
+//   if (result.id !== 3) {
+//     console.error(
+//       'Asked for item with id of `3` but got back one with id of ' + result.id
+//     );
+//     return;
+//   }
+//   if (result.bang !== 'boo') {
+//     console.error(
+//       'Expected all key/value pairs from target object to be returned'
+//     );
+//     return;
+//   }
+//   console.log('SUCCESS: `findByid` is working');
+// }
+  
+// testIt();
+  
