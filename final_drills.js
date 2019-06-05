2f415253beea1082efd66e2562a69f3265468887
Chris Martin <chris.bizunit34@gmail.com>
2019-06-05 15:45:28 -0400

    First Arrays in Objects

--- final_drills.js
+++ final_drills.js
@@ -9,17 +9,26 @@
 // console.log(`${loaf.flour} and ${loaf.water}`);
 // console.log(loaf.hydration());
 
-//Iterating over an object's properties
-const obj={
-  foo:1,
-  bar:2,
-  fum:3,
-  quux:4,
-  spam:5
-};
-let text='';
-let x;
-for (x in obj) {
-  text += x + ' ' + obj[x] + ' ';
-}
-console.log(text);
+// //Iterating over an object's properties
+// const obj={
+//   foo:1,
+//   bar:2,
+//   fum:3,
+//   quux:4,
+//   spam:5
+// };
+// let text='';
+// let x;
+// for (x in obj) {
+//   text += x + ' ' + obj[x] + ' ';
+// }
+// console.log(text);
+
+// //Arrays in objects
+// const hobbitMeal = {
+//   meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
+// };
+
+// console.log(hobbitMeal.meals[3])
+
+//Arrays in objects part II
